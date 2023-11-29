7b3538028610410e1972498f2998759d0c523b41
LetsWorkb1 <jhow.win@gmail.com>
2023-11-28 22:24:53 -0300

    Feat: studying map, weakMap, set, weakSet, reflect and proxy

---
+++ modulo04/aula03-map-e-weakmap/map-e-weakmap.js
@@ -0,0 +1,93 @@
+const assert = require('assert');
+const myMap = new Map();
+
+//Podem ter qualquer coisa como chave
+myMap
+  .set(1, "one")
+  .set('Jhonatan', {text: "two"})
+  .set(true, () => "hello")
+
+//usando um contrutor
+const myMapWiithContrcutor = new Map([
+  ['1', 'str1'],
+  [1, 'num1'],
+  [true, 'bool1']
+])
+
+// console.log('myMap', myMap)
+// console.log('myMap.get(1)', myMap.get(1))
+assert.deepStrictEqual(myMap.get(1), 'one')
+assert.deepStrictEqual(myMap.get('Jhonatan'), {text: "two"})
+assert.deepStrictEqual(myMap.get(true)(),  'hello')
+
+// Em Objects a chave só pode ser string ou symbol (number é coergido a string)
+const onlyReferenceWorks = {id: 1}
+myMap.set(onlyReferenceWorks, {name: 'Jhonatan'})
+
+// console.log('get', myMap.get(onlyReferenceWorks))
+assert.deepStrictEqual(myMap.get({id: 1}), undefined)
+assert.deepStrictEqual(myMap.get(onlyReferenceWorks), {name: "Jhonatan"})
+
+// utilitarios
+// - No Object seria Object.keys({a: 1}).length
+assert.deepStrictEqual(myMap.size, 4)
+
+// para verificar se um item existe no objeto
+// item.key = se não existe = undefined
+// if() = coerçao implicita para boolean e retorna false
+// O jeito certo em Object é ({name : "Jhonatan"}).hasOwnProperty("name")
+assert.ok(myMap.has(onlyReferenceWorks))
+
+// Para remover um item do objeto
+// delete item.id
+// imperformático para o Javascript
+assert.ok(myMap.delete(onlyReferenceWorks))
+
+// Nao dá para iterar em Objects diretamente
+// tem que transformar com Object.entries(item)
+assert.deepStrictEqual(JSON.stringify([...myMap]), JSON.stringify([[1,"one"],["Jhonatan",{"text":"two"}],[true,() => {}]]))
+
+// for (const [key, values] of myMap) {
+// console.log({key, values})
+//}
+
+//Object é inseguro, pois dependendo do nome da chave, pode substituir algum comportamento padrao
+//({ }).toString() === 'óbject Object]'
+// ({toString: () => "Hey"}).toString() === "Hey"
+
+// qualquer chave pode colidir, com as propriedades herdadas do objeto, como
+// constructor, toString, valueOf e etec..
+
+const actor = {
+  name: 'Xuxa da Silva',
+  toString: "Queen: Xuxa da Silva",
+}
+
+//nao tem restricao de nome de chave
+myMap.set(actor)
+
+assert.ok(myMap.has(actor))
+assert.throws(() => myMap.get(actor).toString, TypeError)
+
+// Nao da para limpar um Obj sem reassina-lo
+myMap.clear()
+assert.deepStrictEqual([...myMap.keys()], [])
+
+
+// ---- WeakMap
+
+//Pode ser coletado após perder as referencias
+// usando em casos beem especificos
+
+// tem a maioria dos beneficios do Map
+// Mas: não é interável
+// Só chaves de referencia e que você já conheça
+// mais leve e preve leak de memoria, pq depois que as instancias saem da memoria, tudo é limpo
+
+const weakMap = new WeakMap();
+const hero = {name: 'Flash'}
+
+// weakMap.set(hero)
+// weakMap.get(hero)
+// weakMap.delete(hero)
+// weakMap.has(hero)
